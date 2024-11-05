513727a006cbc7509ee7a02b01948b12d42627b2
RafaMacoto <rafaelmacoto@gmail.com>
2024-11-04 22:24:24 -0300

    pagina conversa e senha completas

--- src/app/conversa/page.tsx
+++ src/app/conversa/page.tsx
@@ -1,8 +1,42 @@
-
-export default function Conversa(){
-
-    return(
-        <main>
-        </main>
-    )
-}
+declare global {
+    interface Window {
+      watsonAssistantChatOptions: {
+        integrationID: string;
+        region: string;
+        serviceInstanceID: string;
+        clientVersion?: string;
+        onLoad: (instance: any) => void;
+      };
+    }
+  }
+  
+  export default function Conversa() {
+  
+    useEffect(() => {
+      // Definindo as opções do Watson Assistant no objeto window
+      window.watsonAssistantChatOptions = {
+        integrationID: "bc79f3b1-ccc9-42d0-97f1-59222ca9e07d", // O ID da integração
+        region: "us-south", // Região onde sua integração está hospedada
+        serviceInstanceID: "be0981d1-7e5f-4b85-978d-e9792df83dea", // ID da instância de serviço
+        onLoad: async (instance: any) => {
+          await instance.render(); // Renderizando o chat na página
+        },
+      };
+  
+      // Carregando o script do Watson Assistant
+      setTimeout(() => {
+        const t = document.createElement('script');
+        t.src = https://web-chat.global.assistant.watson.appdomain.cloud/versions/${
+          window.watsonAssistantChatOptions.clientVersion || 'latest'
+        }/WatsonAssistantChatEntry.js;
+        document.head.appendChild(t);
+      }, 0);
+    }, []);
+  
+    return (
+      <>
+        <H2Conversa>Clique a direita para iniciar a conversa</H2Conversa>
+        <div id="watson-assistant-chat" />
+      </>
+    );
+  }
--- src/app/senha/page.tsx
+++ src/app/senha/page.tsx
@@ -3,6 +3,20 @@ export default function Senha(){
 
     return(
         <main>
+            <div className="div_principal">
+                <div className="principal_textos">
+                    <p className="principal_paragrafo">Insira seu email e clique no botão para obter um link de verificação para redefinir  sua senha.</p>
+                </div>
+                <form action="Redefinir senha" method="get" className="principal_inputs">
+                    <label htmlFor="email_cadastro">Email</label>
+                    <input type="email" name="email" id="email_cadastro" placeholder="me@porto.com" required className="inputs"    autoFocus />
+                    <input type="submit" value="Obter link de verificação" className="submit_botao" />
+
+                </form>
+                <div className="link_volta">
+                    <Link href = "/login" className="voltar">Voltar</Link>
+                </div>
+            </div>
         </main>
     )
 }
